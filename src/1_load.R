6941e698169c34c197eb0908a24d25cacc6b8ee3
mgirdwood7 <mickgirdwood@gmail.com>
2022-10-12 15:17:49 +1100

    update 12 oct

--- src/1_load.R
+++ src/1_load.R
@@ -7,6 +7,6 @@ library(ggmap)
 # Requires google API key for collection of postcode data from free text address field
 # Register google API key with register_google(key = "", write = TRUE)
 
-superk <- read.csv("data/raw/export_230822.csv", header = T, na.strings = "")
+superk <- read.csv("data/raw/export_121022.csv", header = T, na.strings = "")
 varnames <- readxl::read_xlsx("data/raw/superkneedata notes.xlsx", sheet = 1, na = "")
 
